cb3ee2780bf015513ef97248c3b1253950b162fe
Jacob.Wilde <jacob.wilde@utah.edu>
2020-11-05 21:20:12 -0700

    corrected server.js bad merge

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 const express = require('express');
-const path = require('path');
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({ });
 const routes = require('./controllers');
-const router = require('./controllers/home-routes');
+const sequelize = require('./config/connection');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 const exphbs = require('express-handlebars');
 const session = require('express-session');
 const hbs = exphbs.create();
-const path = require('path');
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
