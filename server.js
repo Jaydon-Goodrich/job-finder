553492643f68dc26a18fb3f037df513c970b6550
JoshMiller5994 <talk2joshbgosh@gmail.com>
2020-11-05 19:46:54 -0700

    fixed a bug in the routes

--- server.js
+++ server.js
@@ -3,21 +3,20 @@ const path = require('path');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({ });
 const routes = require('./controllers');
+const router = require('./controllers/home-routes');
 
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.get('/', (req, res) => {
-//     res.send("Hello Job Finder!")
-// })
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
+app.use(routes);
 
 app.listen(PORT, () => {
     console.log(`Now listening on http://localhost:${PORT}`);
