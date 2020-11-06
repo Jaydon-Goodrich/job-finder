30bc1bd78dd4b0f726ddc8ea6db52e4a96b4882b
JoshMiller5994 <talk2joshbgosh@gmail.com>
2020-11-05 19:35:44 -0700

    added the home-routes page.

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ });
+const routes = require('./controllers');
 
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.get('/', (req, res) => {
-    res.send("Hello Job Finder!")
-})
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.get('/', (req, res) => {
+//     res.send("Hello Job Finder!")
+// })
 
 app.listen(PORT, () => {
     console.log(`Now listening on http://localhost:${PORT}`);
