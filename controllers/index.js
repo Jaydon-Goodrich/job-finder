dbe65e5b05e0af61372e9e034700c39e962a54be
Jacob.Wilde <jacob.wilde@utah.edu>
2020-11-05 21:36:26 -0700

    restructure

--- controllers/index.js
+++ controllers/index.js
@@ -1,16 +1,12 @@
 const router = require('express').Router();
-
 const apiRoutes = require('./api');
-
-router.use('/api', apiRoutes);
-
-router.use((req, res) => {
-    res.status(404).end();
-  });
-  
-  module.exports = router;
 const homeRoutes = require('./home-routes');
 
+router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
+
+router.use((req, res) => {
+  res.status(404).end();
+});
 
 module.exports = router;
