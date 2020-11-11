a280eb391503b01a6af0598e9c84662b9ddb226e
Duke Nukem <garrettduke87@gmail.com>
2020-11-10 22:13:30 -0700

    started viewedJobs.js

---
+++ models/viewedJobs.js
@@ -0,0 +1,14 @@
+const { sequelize } = require("./User");
+
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class viewedJobs extends Model {
+
+}
+
+viewedJobs.init(
+    {
+        
+    }
+)
--- server.js
+++ server.js
@@ -32,5 +32,5 @@ app.use(session(sess));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(`Now listening on http://localhost:${PORT}/`));
 });
