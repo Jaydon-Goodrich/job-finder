b1c3fd03924f17d8fb616992bbf0147e3e4ee3ad
Duke Nukem <garrettduke87@gmail.com>
2020-11-11 17:23:26 -0700

    viewedJobs.js

--- models/viewedJobs.js
+++ models/viewedJobs.js
@@ -7,8 +7,33 @@ class viewedJobs extends Model {
 
 }
 
-viewedJobs.init(
+viewedJobs(
     {
-        
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        job_name: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        job_url: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isUrl: true
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'viewedJobs'
     }
-)
+);
+
+module.exports = viewedJobs;
